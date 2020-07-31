cab995c999b58692664ad5f821d727a029b78060
Ben Lee <bendelonlee@gmail.com>
2020-07-31 13:05:32 -0600

    Adds text file database, reading of it, and error handling

--- app/src/main/java/com/example/hangman/MainActivity.java
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     //declare variables
@@ -42,5 +46,40 @@ public class MainActivity extends AppCompatActivity {
         edtInput = (EditText) findViewById(R.id.edtInput);
         txtLettersTried = (TextView) findViewById(R.id.lettersTried);
         txtTriesLeft= (TextView) findViewById(R.id.txtTriesLeft);
+
+        //traverse databse file and populate array list;
+
+        InputStream myInputStream = null;
+        Scanner in = null;
+        String aWord = "";
+        try {
+            myInputStream = getAssets().open("database_file.txt");
+            in = new Scanner(myInputStream);
+            while(in.hasNext()){
+                aWord = in.next();
+                myListOfWords.add(aWord);
+                Toast.makeText(MainActivity.this, aWord, Toast.LENGTH_SHORT).show();
+
+            }
+        } catch(IOException e){
+            Toast.makeText(MainActivity.this,
+                    e.getClass().getSimpleName() + ": " + e.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+        finally {
+            //close scanner
+            if (in != null) {
+                in.close();
+            }
+            try {
+                if (myInputStream != null) {
+                    myInputStream.close();
+                }
+            } catch (IOException e) {
+                Toast.makeText(MainActivity.this,
+                        e.getClass().getSimpleName() + ": " + e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
